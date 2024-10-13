3043c1050da2a1b096e4f48d92037c3c24b3bc09
Ragini Yadav. <101311420+RaagLibr@users.noreply.github.com>
2024-10-14 00:04:09 +0530

    Update Trapping_Rainwater.java

--- ArrayDSA/Trapping_Rainwater.java
+++ ArrayDSA/Trapping_Rainwater.java
@@ -1,5 +1,5 @@
 package ArrayDSA;
-
+ 
 public class Trapping_Rainwater {
 	
 	public static int trappingRainwater(int height[])
@@ -9,19 +9,21 @@ public class Trapping_Rainwater {
 		int leftmax[]= new int[n];
 		leftmax[0]=height[0];
 		
-		for(int i=0;i<n;i++)
+		for(int i=1;i<n;i++)
 		{
 			leftmax[i]=Math.max(height[i], leftmax[i-1]);
 		}
 		
-		
+		//calculate right max boundary array
 		int rightmax[]=new int [n];
-		rightmax[0]=height[n-1];
-		for(int i=n-2;i>0;i--)
+		rightmax[n-1]=height[n-1];
+		for(int i=n-2;i>=0;i--)
 		{
 			rightmax[i]=Math.max(height[i],rightmax[i+1]);
 		}
 		
+		
+		 // Calculate the accumulated water element by element
 		int trappedWater=0;
 		for(int i=0;i<n;i++)
 		{
@@ -36,5 +38,6 @@ public class Trapping_Rainwater {
 	public static void main(String[] args) {
 		int height[]= {4,5,6,3,7,3,9};
 		trappingRainwater(height);
+		System.out.println("Trapping rain water = "+trappingRainwater(height));
 	}
 }
