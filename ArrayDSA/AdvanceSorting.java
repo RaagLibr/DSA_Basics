10ea07d421b12fea8200bf8907e160f0151a47db
Ragini Yadav. <101311420+RaagLibr@users.noreply.github.com>
2024-10-15 12:21:41 +0530

    Add files via upload

---
+++ ArrayDSA/AdvanceSorting.java
@@ -0,0 +1,70 @@
+package ArrayDSA;
+
+public class AdvanceSorting {
+
+	
+	public static int[] countSort(int[] inputArray) {
+       int n=inputArray.length;
+       int M=0;
+       
+       for(int i=0;i<n;i++)
+       {
+    	   M=Math.max(M, inputArray[i]);
+       }
+       
+       int countArray[]= new int[M+1];
+       
+       for(int i=0;i<n;i++)
+       {
+//This increments the count of the element inputArray[i] by 1.
+//So if the element appears multiple times in the inputArray, 
+//each time it's encountered, its corresponding count in the countArray is increased.
+    	   countArray[inputArray[i]]++;
+    	   
+       }
+       
+       for(int i=1;i<=M;i++)
+       {
+    	   countArray[i]+=countArray[i-1];
+       }
+	
+	
+       int outputArray[] = new int[n];
+       for (int i = n - 1; i >= 0; i--) {
+            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+            countArray[inputArray[i]]--;
+
+        }
+       
+       
+        return outputArray;
+    }
+
+	  /* A utility function to print array of size n */
+	   
+	
+	
+
+	public static void main(String[] args) {
+		
+		 int[] arr= {4, 3, 12, 1, 5, 5, 3, 9};
+	       
+        
+        
+       
+        int[] outputArray = countSort(arr);
+        System.out.println("cS");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(outputArray[i]);
+            System.out.println();
+           
+            //OR
+            
+//            System.out.print(outputArray[i] + "c " + arr[i] + "i " + arr[i] + "i " + arr[i] + "i ");
+//            System.out.println(); // Move to the next line after printing
+//            
+         }
+		
+       
+	}
+}
---
+++ ArrayDSA/BinarySearchEx.java
@@ -0,0 +1,37 @@
+package ArrayDSA;
+
+public class BinarySearchEx {
+	public static int binarySearch(int numbers[], int key)
+	{
+		int start=0,end = numbers.length-1;
+		
+		while(start<=end)
+		{
+			int mid=(start+end)/2;
+			
+			//comparisons
+			if(numbers[mid]==key)
+			{
+				return mid;
+			}
+			
+			if(numbers[mid]<key)
+			{
+				start = mid+1;			//right
+			}
+			
+			if(numbers[mid]>key)				
+			{
+				end=mid-1;   			//left
+			}
+		}
+		
+		return -1;
+	}
+	public static void main(String[] args) {
+		int numbers[]= {34,66,73,2,89,5,9};
+		int key=5;
+		
+		System.out.println("index for key is = "+binarySearch(numbers,key));
+	}
+}
